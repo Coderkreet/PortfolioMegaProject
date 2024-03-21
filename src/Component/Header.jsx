7b94f8b93d6522860e6771872b1e5e83df4e1fd4
“[Coderkreet]” <[krunalbende2@gmail.com]>
2024-03-21 22:23:18 +0530

    ok

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,5 +1,5 @@
 import Button from 'react-bootstrap/Button';
-import React from 'react';
+import React, { useState } from 'react';
 // import '..owl.carousel.min.css';
 import "./css/anitabs.css"
 import "./css/style.css"
@@ -16,8 +16,32 @@ import "./css/style-dark.css"
 import OrixLogo from "./img/orix-logo.png"
 import OrixLogoblak from "./img/black-logo.png"
 import { Link } from 'react-router-dom';
+import { FaBars, FaDownload } from 'react-icons/fa';
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isHome, setIshome] = useState(false);
+    const [isPortfolio, setPortfolio] = useState(false);
+    const [isBlog, setBlog] = useState(false);
+    const [isPages, setPages] = useState(false);
+  
+    const toggleMenu = () => {
+      setIsMenuOpen(!isMenuOpen);
+    };
+    const toggleBlog = () => {
+      setBlog(!isBlog);
+    };
+    const togglePages = () => {
+      setPages(!isPages);
+    };
+    const togglehome = () => {
+      setIshome(!isHome);
+    };
+    const togglrPortfolio = () => {
+      setPortfolio(!isPortfolio);
+    };
+
+
     return (
         <header className="two three slideup" id="stickyHeader">
             <div className="container">
@@ -29,7 +53,7 @@ const Header = () => {
                                 <img className="drak" src={OrixLogoblak} alt="logo" />
                             </Link>
                             <Link to="javascript:void(0)" className="bar-menu magic-hover magic-hover__square">
-                                <i className="fa-solid fa-bars"></i>
+                                <i className="fa-solid fa-bars" onClick={toggleMenu} ><FaBars/></i>
                             </Link>
                         </div>
                     </div>
@@ -87,7 +111,7 @@ const Header = () => {
                     <div className="col-xl-3">
                         <div className="extras">
                             <Link to="javascript:void(0)" id="desktop-menu" className="magic-hover magic-hover__square menu-btn">
-                                <i className="fa-solid fa-bars"></i>
+                                <i className="fa-solid fa-bars"> <FaBars/></i>
                             </Link>
                             <Link to="pdf/orix.pdf" className="magic-hover magic-hover__square button download button--moema button--text-thick button--text-upper button--size-s" target="_blank">
                                 <i className="fa-solid fa-download"></i>Download CV
@@ -101,9 +125,181 @@ const Header = () => {
                     </div>
                 </div>
             </div>
-            <div className="mobile-nav" id="mobile-nav" style={{ display: 'block' }}>
-                {/* Mobile navigation content */}
-            </div>
+            <div className={`mobile-nav ${isMenuOpen ? 'open' : ''}`} id="mobile-nav" style={{ display: "block" }}>
+    <div className="mobile-nav-img-data">
+      <a className="magic-hover magic-hover__square" href="index.html">
+        <img alt="logo" src={OrixLogoblak} />
+      </a>
+    </div>
+    <ul>
+    <li className={`menu-item-has-children ${isHome ? 'active' : ''}`} onClick={togglehome} >
+        <Link
+          className="magic-hover magic-hover__square"
+        
+        >
+          <i className="icon-home" /> Home
+        </Link>
+        <ul className="sub-menu">
+          <li>
+            <Link className="magic-hover magic-hover__square" to="/">
+              Home 1
+            </Link>
+          </li>
+          <li>
+            <Link className="magic-hover magic-hover__square" to="/Home_2">
+              Home 2
+            </Link>
+          </li>
+          <li>
+            <Link className="magic-hover magic-hover__square" to="/Home_3">
+              Home 3
+            </Link>
+          </li>
+          <li>
+            <Link className="magic-hover magic-hover__square" to="/Home_4">
+              Home 4 ( Light )
+            </Link>
+          </li>
+          <li>
+            <a
+              className="magic-hover magic-hover__square"
+              href="/"
+            >
+              Home 4 ( Dark )
+            </a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <Link className="magic-hover magic-hover__square" to="/About">
+          <i className="icon-user" /> About
+        </Link>
+      </li>
+      <li className={`menu-item-has-children ${isPortfolio ? 'active' : ''}`} onClick={togglrPortfolio}>
+        <a
+          className="magic-hover magic-hover__square"
+          href="JavaScript:void(0)"
+        >
+          <i className="icon-briefcase" /> Portfolio
+        </a>
+        <ul className="sub-menu">
+          <li>
+            <Link
+              className="magic-hover magic-hover__square"
+              
+              to="Portfolio_1"
+            >
+              Portfolio Details 1
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="magic-hover magic-hover__square"
+              to="Portfolio_2"
+            >
+              Portfolio Details 2
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="magic-hover magic-hover__square"
+
+              href="Portfolio_3"
+            >
+              Portfolio Details 3
+            </Link>
+          </li>
+        </ul>
+      </li>
+      <li className={`menu-item-has-children ${isBlog ? 'active' : ''}`} onClick={toggleBlog} >
+        <Link
+          className="magic-hover magic-hover__square"
+          href="JavaScript:void(0)"
+        >
+          <i className="icon-layers" />
+          Blog
+        </Link>
+        <ul className="sub-menu">
+          <li>
+            <Link className="magic-hover magic-hover__square" to="/OurBlog">
+              Our Blog
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="magic-hover magic-hover__square"
+              to="/BlogDetails_1"
+            >
+              Blog Details 1
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="magic-hover magic-hover__square"
+              to="/BlogDetails_2"
+            >
+              Blog Details 2
+            </Link>
+          </li>
+        </ul>
+      </li>
+      <li className={`menu-item-has-children ${isPages ? 'active' : ''}`} onClick={togglePages} >
+        <Link
+          className="magic-hover magic-hover__square"
+          href="JavaScript:void(0)"
+        >
+          <i className="icon-folder" /> Pages
+        </Link>
+        <ul className="sub-menu">
+          <li>
+            <Link className="magic-hover magic-hover__square" to="/Services">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="magic-hover magic-hover__square"
+              to ="/Services_Details"
+            >
+              Services Details
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="magic-hover magic-hover__square"
+              to="/PricingPlans"
+            >
+              Pricing Plans
+            </Link>
+          </li>
+          <li>
+            <a className="magic-hover magic-hover__square" href="work">
+              404
+            </a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <a className="magic-hover magic-hover__square" href="contact.html">
+          <i className="icon-envelope-open" /> Contact
+        </a>
+      </li>
+    </ul>
+    <a
+      className="magic-hover magic-hover__square"
+      href="JavaScript:void(0)"
+      id="res-cross"
+      onClick={toggleMenu}
+    />
+    <a
+      href="pdf/orix.pdf"
+      className="magic-hover magic-hover__square button download  button--moema button--text-thick button--text-upper button--size-s"
+      target="_blank"
+    >
+      <i className="fa-solid fa-download" > <FaDownload/></i>
+      Download CV
+    </a>
+  </div>
         </header>
     );
 };
